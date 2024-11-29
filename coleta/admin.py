d966e7cc4d596d0136ce8009e7f458eea2d377cd
CodeByDouglas <douglas.ofc.2004@gmail.com>
2024-11-29 03:20:38 -0300

    Registra modelos de coleta orgânica, coleta seletiva e ecopontos no admin do Django e cria a migração inicial para as tabelas correspondentes.

--- coleta/admin.py
+++ coleta/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Coleta_Organica, Coleta_Seletiva, Eco_pontos
 
-# Register your models here.
+admin.site.register(Coleta_Organica )
+admin.site.register(Coleta_Seletiva )
+admin.site.register(Eco_pontos )
